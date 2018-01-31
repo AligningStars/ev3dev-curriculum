2386ce78f3a136a2925e8551d701e1ab488aff40
niruien <nir@rose-hulman.edu>
2018-01-31 14:01:23 -0500

    m1

--- sandbox/m4/motors/m2_drive_encoders.py
+++ sandbox/m4/motors/m2_drive_encoders.py
@@ -4,11 +4,52 @@ This module lets you practice using the encoder to determine distances while blo
 
 You will now use a run_to_rel_pos command to implement the action drive inches action.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Sydney, Rebecca, Cheryl.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-# TODO: 2. Copy the contents of your m1_drive_timed.py and paste that text into this file below these comments.
+# DONE: 2. Copy the contents of your m1_drive_timed.py and paste that text into
+#  this file below these comments.
 #   If your program says and prints anything at the start change it to print and say "Drive using encoders"
+
+import ev3dev.ev3 as ev3
+import time
+
+def main():
+    print("--------------------------------------------")
+    print("  Drive using input")
+    print("--------------------------------------------")
+    ev3.Sound.speak("Drive using input").wait()
+
+    # Connect two large motors on output ports B and C
+    left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+    right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+
+    # Check that the motors are actually connected
+    assert left_motor.connected
+    assert right_motor.connected
+
+    time_s = 1  # Any value other than 0.
+    while time_s != 0:
+        m = 0.01
+        motor_dps = int(input("Enter a speed for the motor (0 to 900 ""dps): "))
+        motor_inps = m * motor_dps
+        dis_in = int(input("Enter a distance to drive (inches): "))
+        time_s = dis_in / motor_inps
+        if motor_dps == 0 or dis_in == 0:
+            break
+
+        left_motor.run_forever(speed_sp=motor_dps, time_sp=time_s)
+        right_motor.run_forever(speed_sp=motor_dps, time_sp=time_s)
+        time.sleep(time_s)
+        left_motor.stop()
+        right_motor.stop(stop_action="brake")
+
+    ev3.Sound.beep().wait()
+    print("Goodbye!")
+    ev3.Sound.speak("Goodbye").wait()
+
+
+main()
 
 # TODO: 3. Add a beep after the drive motors stop (see code below).  Test your code to hear the beep AFTER movement.
 #   ev3.Sound.beep().wait()
