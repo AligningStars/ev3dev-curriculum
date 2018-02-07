beba2c5bc8b2d6b890ea2b7a8a5140b5873b5d9b
niruien <nir@rose-hulman.edu>
2018-02-07 15:04:16 -0500

    color sensors & beacon

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -25,6 +25,7 @@ class Snatch3r(object):
         self.arm_motor = ev3.MediumMotor(ev3.OUTPUT_A)
         self.touch_sensor = ev3.TouchSensor()
         self.color_sensor = ev3.ColorSensor()
+        self.ir_sensor = ev3.InfraredSensor
         self.MAX_SPEED = 900
 
         # Check that the motors are actually connected
@@ -33,6 +34,7 @@ class Snatch3r(object):
         assert self.arm_motor
         assert self.touch_sensor
         assert self.color_sensor
+        assert self.ir_sensor
 
     def drive_inches(self, inches_target, motor_dps):
         """Drive the desired number of inches as inputed by the user,
