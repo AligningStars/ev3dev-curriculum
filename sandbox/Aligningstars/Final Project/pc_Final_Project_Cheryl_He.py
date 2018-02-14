f5735d4fe5b1e8b9ee140fd5d06c1e3846b74ed2
Cheryl <hey8@rose-hulman.edu>
2018-02-14 14:57:26 -0500

    pc

--- sandbox/Aligningstars/Final Project/pc_Final_Project_Cheryl_He.py
+++ sandbox/Aligningstars/Final Project/pc_Final_Project_Cheryl_He.py
@@ -19,5 +19,15 @@ def main():
     root = tkinter.Tk()
     root.title("MQTT Remote")
 
-    main_frame = ttk.Frame(root, padding=20, relief='raised')
+    main_frame = ttk.Frame(root, padding=40, relief='raised')
     main_frame.grid()
+
+
+    word = 'project'
+    for k in range(len(word)):
+        letter = ttk.Label(main_frame, text="____")
+        letter.grid(row=1, column=k)
+        letter_entry = ttk.Entry(main_frame, width=4)
+        letter_entry.grid(row=0, column=k)
+
+    root.mainloop()
